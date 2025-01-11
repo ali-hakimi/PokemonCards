78b29b67b980d92c190fb7e5aff17f291b863aea
Ali <alihakimi014@gmail.com>
2025-01-11 15:48:57 -0700

    Step 2: Searching on List Page

--- src/components/PokemonList/PokemonList.tsx
+++ src/components/PokemonList/PokemonList.tsx
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { createUseStyles } from 'react-jss';
 import { useGetPokemons } from '../../hooks/useGetPokemons';
 import { PokemonCard } from '../../components';
 
 export const PokemonList = () => {
   const classes = useStyles();
+  const [searchQuery, setSearchQuery] = useState('');
   const { pokemons, loading } = useGetPokemons();
 
+  const filteredPokemons = pokemons.filter(pkmn =>
+    pkmn.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+  
   return (
     <div className={classes.root}>
-      {loading && <div>Loading...</div>}
+      <input
+        type="text"
+        className={classes.searchInput}
+        placeholder="Search Pokemon"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+      />
       <div className={classes.cardContainer}>
-        {pokemons.map((pkmn, index) => (
+        {filteredPokemons.map((pkmn, index) => (
           <PokemonCard key={index} {...pkmn} />
         ))}
       </div>
@@ -25,6 +36,14 @@ const useStyles = createUseStyles(
       display: 'flex',
       flexDirection: 'column',
       alignItems: 'center',
+    },
+    searchInput: {
+      margin: '16px',
+      padding: '8px',
+      fontSize: '16px',
+      width: '300px',
+      background: '#ffd6ee',
+      color: '#000',
     },
     cardContainer: {
       display: 'flex',
